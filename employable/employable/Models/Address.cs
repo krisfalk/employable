275311c422e4845ab211d4b0a2251f7d58459af6
hilaryberigan <berigan.hilary@gmail.com>
2016-10-04 07:57:16 -0500

    initial upload into new repository that includes gitignore and readme

---
+++ employable/employable/Models/Address.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace employable.Models
+{
+    public class Address
+    {
+        public Address()
+        {
+
+        }
+        [Key]
+        public int Address_Id { get; set; }
+        public int? AptNumber { get; set; }
+        public int HouseNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
---
+++ employable/employable/Models/Resume.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace employable.Models
+{
+    public class Resume
+    {
+        [Key]
+        public int ResumeId { get; set; }
+        [ForeignKey("ApplicationUser")]
+        public string UserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string Experience { get; set; }
+        public string Education { get; set; }
+        public string Skills { get; set; }
+        public string References { get; set; }
+
+    }
+}
